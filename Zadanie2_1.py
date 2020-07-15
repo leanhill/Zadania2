ba4b746d64a78d74310076f7aa8b6cee9b61ae29
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-07-15 23:03:52 +0200

    Zadanie poprawione

--- Zadanie2_1.py
+++ Zadanie2_1.py
@@ -3,9 +3,9 @@
 create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]) # => returns "(+12) 345-678-901"""
 
 def create_phone_number(phone_number_digits):
-    phone_number_digits = str(phone_number_digits).strip("[]")
-    phone_number_digits = phone_number_digits.replace(", ","")
-    phone_number = f"(+{phone_number_digits[:2]}) {phone_number_digits[2:5]}-{phone_number_digits[5:8]}-{phone_number_digits[8:11]}"
+    phone_number = f"(+{phone_number_digits[0]}{phone_number_digits[1]}) {phone_number_digits[2]}{phone_number_digits[2]}" \
+                   f"{phone_number_digits[3]}-{phone_number_digits[5]}{phone_number_digits[6]}{phone_number_digits[7]}" \
+                   f"-{phone_number_digits[8]}{phone_number_digits[9]}{phone_number_digits[10]}"
     return phone_number
 
 
