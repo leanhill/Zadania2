a44142fdf3a8d0705ff681662bb0b9ed71ecc25f
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-07-15 23:39:01 +0200

    Zadanie poprawione

--- Zadanie2_2.py
+++ Zadanie2_2.py
@@ -3,11 +3,18 @@
 
 to_weird_case('String') # => zwraca 'StRiNg'
 to_weird_case('Algorytmy i struktury danych') # => zwraca 'AlGoRyTmY I StRuKtUrY DaNyCh"""
-def to_weird_case(word_toget_weird):
+def to_weird_case(word):
     weird_word = str()
-    for i in range (len(word_toget_weird)):
-        if i%2 == 0:
-            weird_word += word_toget_weird[i].upper()
+    a = 1
+    for character in word:
+        if character == " ":
+            weird_word += character
+            pass
         else:
-            weird_word += word_toget_weird[i].lower()
+            if a%2 == 0:
+                weird_word += character.lower()
+            else:
+                weird_word += character.upper()
+            a += 1
     return weird_word
+
