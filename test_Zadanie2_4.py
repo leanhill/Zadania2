412fa8c0a6e5dad3d38eff7cbc7a77d06098c373
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-07-20 21:40:56 +0200

    Zadanie poprawione - testowanie zmienione na testowanie z parametryzacją

--- test_Zadanie2_4.py
+++ test_Zadanie2_4.py
@@ -1,3 +1,8 @@
 from Zadanie2_4 import bubble_sort
-def test_bubble_sort():
-    assert bubble_sort("9876") == ["6", "7", "8", "9"]
+import pytest
+
+
+@pytest.mark.parametrize("n, result", [("9876", ["6", "7", "8", "9"]), ("155849", ["1", "4", "5", "5", "8", "9"]),
+                                       ("888881", ["1", "8", "8", "8", "8", "8"])])
+def test_bubble_sort(n, result):
+    assert bubble_sort(n) == result
