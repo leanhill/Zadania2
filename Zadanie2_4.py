9b5642dac57259d7353a28c4c571189a32a7de08
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-07-20 22:12:32 +0200

    Zadanie poprawione

--- Zadanie2_4.py
+++ Zadanie2_4.py
@@ -1,7 +1,7 @@
 
 """4. Zaimplementuj sortowanie bąbelkowe. W programowaniu problem sortowań jest jednym z ważniejszych problemów,
 ponieważ każdą daną da się przedstawić jako cyfrę
- (zerknij sobie tak w ramach świadomości na tablekę na tej stronie:
+ (zerknij sobie tak w rssamach świadomości na tablekę na tej stronie:
   http://www.asciitable.com/ ) i zauważ że litery mają swoje odpowiedniki w liczbach :D)
 Załączam również link który opisuje jak ono działa:
  http://www.algorytm.edu.pl/algorytmy-maturalne/sortowanie-babelkowe.html"""
